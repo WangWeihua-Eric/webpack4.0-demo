1192770d9eaefa3890650dcbe1a8165c213754d1
weihua.wang <weihua.wang@qunar.com>
2019-05-05 22:20:11 +0800

    html-webpack-plugin、clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+//plugin可以在webpack运行到某一时刻，帮你做一些事情
 
 module.exports = {
     mode: "development",
@@ -32,6 +36,12 @@ module.exports = {
             ]
         }]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin()
+    ],
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, 'dist')
